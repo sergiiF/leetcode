4db959474da0765b0bbefd70da51127c96cf2f20
Sergii Kashaba <skashaba@mirantis.com>
2018-05-14 22:56:31 -0700

    test

--- inorderTraversal.cpp
+++ inorderTraversal.cpp
@@ -9,7 +9,7 @@ struct TreeNode {
 };
 
 // Inorder (Left, Root, Right)
-#define RECULSION
+#define NORECURSION
 
 
 #ifdef RECURSION
@@ -43,9 +43,40 @@ public:
 
 
 #ifdef NORECURSION
+#include <set>
+#include <stack>
+using namespace std;
+
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s;
+        set<int> marked;
+        vector<int> v;
+
+        if (!root) return v;
+
+        s.push(root);
+        while (!s.empty()) {
+            TreeNode* node = s.top(); s.pop();
+
+            if ((!node->left && !node->right) || (marked.find(node->val) != marked.end())) {
+                v.push_back(node->val);
+                continue;
+            }
+            if (node->right) {
+                s.push(node->right);
+            }
+
+            s.push(node);
+            marked.insert(node->val);
+
+            if(node->left) {
+                s.push(node->left);
+            }
+
+        }
+        return v;
 
     }
 };
